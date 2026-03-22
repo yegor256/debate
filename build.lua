14d87b82a1abdb65010c647ab35d65609b27c0d3
Yegor Bugayenko <yegor256@gmail.com>
2026-03-22 18:14:37 +0300

    version with date

--- build.lua
+++ build.lua
@@ -9,7 +9,7 @@ tagfiles = {"build.lua", "debate.dtx"}
 
 uploadconfig = {
   pkg = "debate",
-  version = "0.0.0",
+  version = "0.0.0 0000/00/00",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
